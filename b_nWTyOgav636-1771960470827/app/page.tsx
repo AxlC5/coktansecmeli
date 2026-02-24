6ffd66b51fd34d13b1307f5db64ef1c26ae0b68d
Mert <103221662+AxlC5@users.noreply.github.com>
2026-02-24 23:06:12 +0300

    Delete b_nWTyOgav636-1771960470827 directory

--- b_nWTyOgav636-1771960470827/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { InteractiveVideoEditor } from "@/components/interactive-video/interactive-video-editor"
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-background">
-      <InteractiveVideoEditor />
-    </main>
-  )
-}
